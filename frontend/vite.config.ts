005743f60cb62fa513d160361f88d3fa6b024e56
1WFT1 <125677570+1WFT1@users.noreply.github.com>
2026-04-07 18:10:03 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: true
+    sourcemap: false
   }
 })
